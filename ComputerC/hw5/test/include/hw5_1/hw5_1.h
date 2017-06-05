09622c88203b815c1860d49e5e99e82231cc3a91
taeyeop kim <ktim610@gmail.com>
2017-06-05 16:04:35 +0900

    update hw5_1

--- ComputerC/hw5/test/include/hw5_1/hw5_1.h
+++ ComputerC/hw5/test/include/hw5_1/hw5_1.h
@@ -6,8 +6,15 @@
  * hw5_1 is responsible for ...
  *
  **********************************************************/
+typedef struct Car_s {
+    char name[20];
+    int model;
+    int count;
+} Car_t;
 
-void hw5_1_create(void);
+void hw5_1_create(Car_t *p_car);
 void hw5_1_destroy(void);
+int add_car(Car_t *p_car);
+int find_car(Car_t *p_car);
 
 #endif  /* D_Fakehw5_1_H */
--- ComputerC/hw5/test/tests/hw5_1/hw5_1Test.cpp
+++ ComputerC/hw5/test/tests/hw5_1/hw5_1Test.cpp
@@ -4,12 +4,15 @@ extern "C"
 }
 
 #include "CppUTest/TestHarness.h"
+#include <stdio.h>
+
+Car_t car[100];
 
 TEST_GROUP(hw5_1)
 {
     void setup()
     {
-        hw5_1_create();
+        hw5_1_create(car);
     }
 
     void teardown()
@@ -18,8 +21,19 @@ TEST_GROUP(hw5_1)
     }
 };
 
-TEST(hw5_1, create)
-{
-    FAIL("Start here");
+TEST(hw5_1, add_car_when_overlap_name_return_idx) {
+    printf("add_car\n");
+
+    CHECK_EQUAL(0, add_car(car));
+    CHECK_EQUAL(1, add_car(car));
 }
 
+TEST(hw5_1, add_car_when_car_name_return_idx) {
+    printf("find_car\n");
+
+    CHECK_EQUAL(0, add_car(car));
+    CHECK_EQUAL(1, add_car(car));
+
+    CHECK_EQUAL(0, find_car(car));
+    CHECK_EQUAL(1, find_car(car));
+}
